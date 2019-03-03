14db8261326561e53479f8405d05eceab9d0b131
Michal Skora <skorek.michal@gmail.com>
2019-03-03 13:16:28 +0100

    Updates

---
+++ precache-manifest.236befd9cb079fd9fb8228cc70bd34f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36b5651f1628d9e8bb8217fe0cb17f83",
+    "url": "/Johnny_Promil_v2/static/media/580b57fbd9996e24bc43c091.36b5651f.png"
+  },
+  {
+    "revision": "136fe05c6fb3f51a464b9e8f2b6ea02f",
+    "url": "/Johnny_Promil_v2/static/media/580b57fbd9996e24bc43c090.136fe05c.png"
+  },
+  {
+    "revision": "17d4ceeb519dbda17bd9",
+    "url": "/Johnny_Promil_v2/static/js/runtime~main.17d4ceeb.js"
+  },
+  {
+    "revision": "1cf79ed1f7421232ffd1",
+    "url": "/Johnny_Promil_v2/static/js/main.1cf79ed1.chunk.js"
+  },
+  {
+    "revision": "9ad126af5b1bc081a72e",
+    "url": "/Johnny_Promil_v2/static/js/2.9ad126af.chunk.js"
+  },
+  {
+    "revision": "1cf79ed1f7421232ffd1",
+    "url": "/Johnny_Promil_v2/static/css/main.1970539e.chunk.css"
+  },
+  {
+    "revision": "165bab63b92ef9437ce48fd2bbc6a20a",
+    "url": "/Johnny_Promil_v2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Johnny_Promil_v2/precache-manifest.b9a0b9ef0e62cb7775f6d8d0ec2a8099.js"
+  "/Johnny_Promil_v2/precache-manifest.236befd9cb079fd9fb8228cc70bd34f6.js"
 );
 
 workbox.clientsClaim();
