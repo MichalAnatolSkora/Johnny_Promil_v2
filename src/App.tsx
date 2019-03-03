6c1954b831f2dc6028fcb67510abf22de2822255
Michal Skora <skorek.michal@gmail.com>
2019-03-03 08:47:28 +0100

    added jumbotron

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, ButtonToolbar, Jumbotron, Container, Row, Navbar, Nav, Form, FormControl, NavDropdown } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +7,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+
+
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Johnny Promil</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse>
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Główna</Nav.Link>
+                <Nav.Link href="#link">Oferta</Nav.Link>
+                <Nav.Link href="#kontakt">Kontakt</Nav.Link>
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown> */}
+              </Nav>
+              {/* <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form> */}
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Jumbotron fluid>
+          <Container>
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Container>
+        </Jumbotron>
       </div>
     );
   }
