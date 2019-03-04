b56469714d0900c70bfeb475cc5240f406467554
Michal Skora <skorek.michal@gmail.com>
2019-03-04 21:31:13 +0100

    next step

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Button, Card, Jumbotron, Container, CardDeck, Navbar, Nav, Form, FormControl, NavDropdown } from 'react-bootstrap';
+import { Button, Card, Jumbotron, Container, CardDeck, Navbar, Nav, Image, FormControl, NavDropdown } from 'react-bootstrap';
 import './App.css';
 import twoBeers from './580b57fbd9996e24bc43c090.png'
 import oneBeer from './580b57fbd9996e24bc43c091.png'
+import beerToGlass from './beer-820011_640.jpg'
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps"
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     const headerJumbotronStyle = {
       backgroundColor: "transparent",
       paddingTop: "80px",
+      paddingBottom: "20px",
       margin: "0px"
     };
 
@@ -26,11 +28,9 @@ class App extends Component {
       margin: "0px"
     };
 
-    const GoogleMapComponent = withScriptjs(withGoogleMap((props) => <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: -34.397, lng: 150.644 }}  >
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    </GoogleMap>));
+    const cardStyle = {
+      borderWidth: '3px'
+    }
 
     return (
       <div className="App">
@@ -51,29 +51,38 @@ class App extends Component {
 
         <Jumbotron style={headerJumbotronStyle} fluid>
           <Container>
-            <Card bg="light" border="warning" text="white" className="text-center p-4">
-
+            <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
+              <div className="grid-container">
+                <div><Image src={beerToGlass} style={{ width: '70%' }} rounded/></div>
+                <div>
+                  <h1>50+</h1>
+                  <p>Piw kraftowych</p>
+                </div>
+              </div>
             </Card>
           </Container>
         </Jumbotron>
 
-        <Jumbotron style={headerJumbotronStyle} fluid>
+        <Jumbotron style={jumbotronStyle} fluid>
           <Container>
-            <iframe width="100%" height="400px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d626.1522285232337!2d17.05488071135112!3d51.11567543726553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9d3d97cf061%3A0xc08116d71816dd97!2sG%C3%B3rnickiego+6%2C+50-337+Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1551622751662" frameBorder="0" style={{ border: "2" }} allowFullScreen></iframe>
+            <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
+              <h1>Odwiedź nas na Górnickiego!</h1>
+              <iframe width="100%" height="400px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d626.1522285232337!2d17.05488071135112!3d51.11567543726553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fe9d3d97cf061%3A0xc08116d71816dd97!2sG%C3%B3rnickiego+6%2C+50-337+Wroc%C5%82aw!5e0!3m2!1spl!2spl!4v1551622751662" frameBorder="0" style={{ paddingTop: '5px', border: "2px" }} allowFullScreen></iframe>
+            </Card>
           </Container>
         </Jumbotron>
 
         <Jumbotron style={jumbotronStyle} fluid>
           <Container>
             <CardDeck>
-              <Card bg="light" text="white" className="text-center p-4">
+              <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
                 <Card.Img src={twoBeers}></Card.Img>
                 Text
             </Card>
-              <Card bg="light" text="white" className="text-center p-4">
+              <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
                 <Card.Img src={oneBeer} variant="bottom" style={{ width: '5rem' }}></Card.Img>
               </Card>
-              <Card bg="light" text="white" className="text-center p-4">
+              <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
                 Text
             </Card>
             </CardDeck>
@@ -83,15 +92,15 @@ class App extends Component {
         <Jumbotron style={jumbotronStyle} fluid>
           <Container>
             <CardDeck>
-              <Card bg="light" text="white" className="text-center p-4">
+              <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
                 <Card.Img src={twoBeers}></Card.Img>
                 Text
             </Card>
-              <Card bg="light" text="white" className="text-center p-1">
+              <Card bg="light" border="warning" style={cardStyle} className="text-center p-1">
                 <Card.Img src={oneBeer} style={{ width: '5rem' }}></Card.Img>
                 Text
             </Card>
-              <Card bg="light" text="white" className="text-center p-4">
+              <Card bg="light" border="warning" style={cardStyle} className="text-center p-4">
                 Text
             </Card>
             </CardDeck>
