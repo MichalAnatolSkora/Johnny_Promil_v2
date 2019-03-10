e389814e4756ba1cf7305108a3138e28b56b9b16
Michal Skora <skorek.michal@gmail.com>
2019-03-10 12:11:44 +0100

    footer + navbar fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Button, Card, Jumbotron, Container, CardDeck, Navbar, Nav, Image, FormControl, NavDropdown } from 'react-bootstrap';
+import { Button, Card, Jumbotron, Container, CardDeck, Navbar, Nav, Image, FormControl, NavDropdown, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import './App.css';
 import { FaBeer } from 'react-icons/fa';
+import { DiGithubBadge, DiReact } from "react-icons/di";
 import { HashRouter, Route, Link, NavLink } from "react-router-dom";
 
 import Index from './subSites/Index';
@@ -36,9 +37,8 @@ class App extends React.Component<any, any> {
       backgroundColor: "#e6e6e6",
       padding: "20px",
       margin: "0px",
-      bottom: "0",
-      position: "absolute",
-      width: "100%"
+      width: "100%",
+      textAlign: "center"
     } as React.CSSProperties;
 
     const cardStyle = {
@@ -48,16 +48,16 @@ class App extends React.Component<any, any> {
     return (
       <HashRouter>
         <>
-          <div className="App">
+          <div className="App page-wrap">
             <Navbar onToggle={(b: boolean) => this.setState({ expanded: b })} expanded={this.state.expanded} fixed='top' bg="dark" variant="dark" expand="lg">
               <Container>
-                <NavLink className="nav-link" to="/"><Button variant="danger"><FaBeer/> Johnny Promil</Button></NavLink>
+                <NavLink className="nav-link" to="/"><Button variant="danger"><FaBeer /> Johnny Promil</Button></NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse appear={true}>
                   <Nav className="mr-auto justify-content-end">
-                    <NavLink className="nav-link" to="/"><Button onClick={() => this.setState({ expanded: false })} size="sm" variant="warning">Główna</Button></NavLink>
-                    <NavLink className="nav-link" to="/Oferta"><Button onClick={() => this.setState({ expanded: false })} size="sm" variant="warning">Oferta</Button></NavLink>
-                    <NavLink className="nav-link" to="/Sklepy"><Button onClick={() => this.setState({ expanded: false })} size="sm" variant="warning">Sklepy</Button></NavLink>
+                    <NavLink onClick={() => this.setState({ expanded: false })} className="nav-link" to="/"><Button size="sm" variant="warning">Główna</Button></NavLink>
+                    <NavLink onClick={() => this.setState({ expanded: false })} className="nav-link" to="/Oferta"><Button size="sm" variant="warning">Oferta</Button></NavLink>
+                    <NavLink onClick={() => this.setState({ expanded: false })} className="nav-link" to="/Sklepy"><Button size="sm" variant="warning">Sklepy</Button></NavLink>
                   </Nav>
                 </Navbar.Collapse>
               </Container>
@@ -66,13 +66,19 @@ class App extends React.Component<any, any> {
             <Route path="/" exact component={Index} />
             <Route path="/Oferta/" component={Products} />
             <Route path="/Sklepy/" component={Shops} />
-
-            <Jumbotron style={footerJumbotronStyle2} fluid>
-              <Container>
-              Footer
-              </Container>
-          </Jumbotron>
           </div>
+          <Jumbotron className="site-footer" style={footerJumbotronStyle2} fluid>
+            <Container>
+              <h3>Powered by:
+                <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">GitHub</Tooltip>}>
+                  <DiGithubBadge />
+                </OverlayTrigger>
+                <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">React</Tooltip>}>
+                  <DiReact />
+                </OverlayTrigger>
+              </h3>
+            </Container>
+          </Jumbotron>
         </>
       </HashRouter>
     );
